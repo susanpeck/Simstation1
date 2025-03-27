b6f742f9dd327d63fc330000db31796ffa90c327
susanpeck <funkyredshoe@gmail.com>
2025-03-27 16:17:15 -0700

    paintComponent

--- src/simstation/WorldView.java
+++ src/simstation/WorldView.java
@@ -9,6 +9,7 @@ import mvc.Model;
 import mvc.View;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 public class WorldView extends View{
     private final World mf;
@@ -40,10 +41,22 @@ public class WorldView extends View{
     }
 
     public void paintComponent(Graphics gc){
+        for(Agent a : ){
+            drawAgent(a, gc);
+        }
 
     }
 
     public void drawAgent(Agent a, Graphics gc){
+        // marked off in Turtle Graphics for View accessing or changing things it shouldn't
 
+        // draw a diameter 10 red filled oval at Agents a's location
+        int xCoordinate = a.getXc();
+        int yCoordinate = a.getYc();
+        Graphics2D g2d = (Graphics2D)gc;
+        Ellipse2D.Double circle = new Ellipse2D.Double(xCoordinate-5, yCoordinate-5, 10, 10);
+        g2d.setColor(Color.RED);
+        g2d.fill(circle);
+        g2d.draw(circle);
     }
 }
