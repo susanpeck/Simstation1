c6666e3afa5b9af86cac864f5e70f0b328e4f8de
Jun <133283143+smol-derp@users.noreply.github.com>
2025-04-05 20:38:58 -0700

    Fixed new not working

--- src/simstation/WorldView.java
+++ src/simstation/WorldView.java
@@ -12,42 +12,22 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class WorldView extends View{
-    private final World worldModel;
+    private static final int AGENT_DIAMETER = 10;
 
     public WorldView(Model m){
         super(m);
         if (model == null || !(model instanceof World)) {
             throw new IllegalStateException("Model must be a World");
         }
-        this.worldModel = (World) model;
         this.setBackground(Color.GRAY);
-        initView();
     }
 
-    private void initView() {
-        //will we need an initView for this project?
-    }
-
-    @Override
-    public void update(){
-        repaint();
-    }
-
-    public void setModel(Model newModel) {
-        super.setModel(newModel);
-        //initView();
-        repaint();
-    }
 
     public void paintComponent(Graphics gc){
         super.paintComponent(gc);
-        Graphics2D g2d = (Graphics2D) gc;
-
         //paint all the agents in the World
-        synchronized(worldModel) {
-            for(Agent a : worldModel.getAgents()){
-                drawAgent(a, g2d);
-            }
+        for(Agent a : ((World)model).getAgents()){
+            drawAgent(a, (Graphics2D)gc);
         }
     }
 
@@ -57,7 +37,10 @@ public class WorldView extends View{
         // draw a diameter 10 red filled oval at Agents a's location
         int xCoordinate = a.getXc();
         int yCoordinate = a.getYc();
-        Ellipse2D.Double circle = new Ellipse2D.Double(xCoordinate-5, yCoordinate-5, 10, 10);
+        Ellipse2D.Double circle = new Ellipse2D.Double(xCoordinate - (AGENT_DIAMETER/2),
+                                                        yCoordinate - (AGENT_DIAMETER/2),
+                                                        AGENT_DIAMETER,
+                                                        AGENT_DIAMETER);
         gc.setColor(Color.RED);
         gc.fill(circle);
         gc.draw(circle);
