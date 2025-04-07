2a6c310487b61a8a537aecb84b7367e20627303c
Jun <133283143+smol-derp@users.noreply.github.com>
2025-04-07 00:54:38 -0700

    finished logic

--- src/simstation/plague/Creature.java
+++ src/simstation/plague/Creature.java
@@ -1,6 +1,72 @@
 package simstation.plague;
 
-import simstation.MobileAgent;
+import mvc.*;
+import simstation.*;
 
 public class Creature extends MobileAgent {
+    private boolean isInfected;
+    private int recoveryOrFatalityTime;
+    private boolean isFatal;
+    private int time;
+    private boolean isDead;
+
+    static {
+        sleepTime = 20;
+    }
+
+    public Creature(int recoveryOrFatalityTime, boolean isFatal, boolean isInfected) {
+        this.recoveryOrFatalityTime = recoveryOrFatalityTime;
+        this.isFatal = isFatal;
+        this.isInfected = isInfected;
+        this.isDead = false;
+        this.time = 0;
+    }
+
+    @Override
+    // might be an issue with the move logic since a random agent would just freeze, might be something wrong with the heading
+    // also noticed that sometimes agents would disappear so the wrap logic might need some fixing
+    public void update() {
+        if (!this.isDead) {
+            PlagueSim plague = (PlagueSim)world;
+            Creature c = (Creature)world.getNeighbor(this, 10);
+            if (c != null && !this.isInfected && c.isInfected) {
+                // chance of infection
+                int rand = Utilities.rng.nextInt(100);
+                if (rand < plague.getVIRULENCE()) {
+                    rand = Utilities.rng.nextInt(100);
+                    if (rand > plague.getRESISTANCE()) {
+                        isInfected = true;
+                        plague.setINFECTED(plague.getINFECTED() + 1);
+                        time = 0;
+                        this.recoveryOrFatalityTime = c.getRecoveryOrFatalityTime();
+                    }
+                }
+            }
+
+            if (isInfected) {
+                time++;
+            }
+
+            if (time >= recoveryOrFatalityTime) {
+                if (this.isFatal) {
+                    isDead = true;
+                }
+                isInfected = false;
+                plague.setINFECTED(plague.getINFECTED() - 1);
+            }
+            move(1);
+        }
+    }
+
+    public boolean isInfected() {
+        return isInfected;
+    }
+
+    public int getRecoveryOrFatalityTime() {
+        return recoveryOrFatalityTime;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
 }
