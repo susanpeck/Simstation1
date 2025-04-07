6c90fecdb3977d61c0d0c0d252eab2a3285a75bd
Jun <133283143+smol-derp@users.noreply.github.com>
2025-04-06 21:37:52 -0700

    Updated about info and commands

--- src/simstation/plague/PlagueFactory.java
+++ src/simstation/plague/PlagueFactory.java
@@ -1,14 +1,37 @@
 package simstation.plague;
 
-import mvc.Model;
-import mvc.View;
-import simstation.World;
-import simstation.WorldFactory;
+import mvc.*;
+import simstation.*;
 
 public class PlagueFactory extends WorldFactory {
     public Model makeModel() { return new PlagueSim(); }
+
     public View makeView(Model m) {
         return new PlagueView((World) m);
     }
+
     public String getTitle() { return "Plague";}
+
+    public String[] getHelp() {
+        return new String[] {
+                "Start - populates simulation with agents",
+                "Pauses - pauses agents",
+                "Resume - starts the paused agents",
+                "Stop - stops agents",
+                "Stats - shows stats of agents",
+                "Not Fatal - changes fatality time to recovery time",
+        };
+    }
+
+    public Command makeEditCommand(Model model, String type, Object source) {
+        return switch (type) {
+            case "Start" -> new StartCommand(model);
+            case "Pause" -> new PauseCommand(model);
+            case "Resume" -> new ResumeCommand(model);
+            case "Stop" -> new StopCommand(model);
+            case "Stats" -> new StatsCommand(model);
+            case "Not Fatal" -> new FatalCommand(model);
+            default -> null;
+        };
+    }
 }
