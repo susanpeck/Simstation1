9adb7a96b655e79172f5c5638acd9d930b59d815
Jun <133283143+smol-derp@users.noreply.github.com>
2025-04-06 21:37:06 -0700

    Created Fatal Command

---
+++ src/simstation/plague/FatalCommand.java
@@ -0,0 +1,18 @@
+package simstation.plague;
+
+import mvc.Command;
+import mvc.Model;
+
+public class FatalCommand extends Command {
+    public FatalCommand(Model model) {
+        super(model);
+    }
+
+    @Override
+    public void execute() throws Exception {
+        if (!(model instanceof PlagueSim plauge)) {
+            throw new Exception("Model must be a World.");
+        }
+        plauge.setFatality(!plauge.isFatal());
+    }
+}
