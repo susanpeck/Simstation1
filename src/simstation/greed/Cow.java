2f16ac41257e4cbc86622611b1ad5ed413fa1fe3
Jun <133283143+smol-derp@users.noreply.github.com>
2025-04-09 12:43:16 -0700

    Initial Greed

---
+++ src/simstation/greed/Cow.java
@@ -0,0 +1,14 @@
+package simstation.greed;
+
+import simstation.MobileAgent;
+
+public class Cow extends MobileAgent {
+    private int energy = 100;
+    public static int greediness = 25;
+    private Meadow location;
+
+    @Override
+    public void update() {
+        super.update();
+    }
+}
