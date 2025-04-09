9b3fdcc6fb147ad29f080450446f9457c2fa8083
Jun <133283143+smol-derp@users.noreply.github.com>
2025-04-09 12:55:47 -0700

    added prof notes on greed

--- src/simstation/greed/Cow.java
+++ src/simstation/greed/Cow.java
@@ -7,6 +7,9 @@ public class Cow extends MobileAgent {
     public static int greediness = 25;
     private Meadow location;
 
+    // goes to location and calls eatMe, tries to top off energy to 100, eating grass depletes energy of patch
+    // several cows can occupy a single patch of grass but every cow can't eat at the same time (bank account situation)
+    // patch needs to be threat safe, eatMe is synchronized?
     @Override
     public void update() {
         super.update();
