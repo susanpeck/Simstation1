483c407ea341f46c46d5b20b7279c909b0f746c5
Jun <133283143+smol-derp@users.noreply.github.com>
2025-04-09 12:55:47 -0700

    added prof notes on greed

--- src/simstation/greed/Patch.java
+++ src/simstation/greed/Patch.java
@@ -16,7 +16,21 @@ public class Patch extends Agent {
 
     }
 
-    public void eatMe(Cow cow, int amt) {
-
+    public synchronized void eatMe(Cow cow, int amt) {
+        // what prof had
+//        if(cow.isStopped()) return;
+//        while (energy < amt) {
+//            try {
+//                wait();
+//                //cow.decEnergy(Meadow.waitPenalty);
+//                if (cow.isStopped()) break;
+//            }
+//            catch (InterruptedException e) {
+//                System.err.println(e.getMessage());
+//            }
+//            if (cow.energy???) {
+//                do stuff lmao i didnt get everything
+//            }
+//        }
     }
 }
